e606a2af47aafe0c3dd05894e22226e48d967ea4
lamabas <dblam.hcmus@gmail.com>
2026-03-03 23:17:56 +0700

    Rewrite insertionSort.cpp

---
+++ sortAlgorithms/insertionSort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+void insertionSort(std::vector<int>& array) {
+    int n = array.size();
+    for (int i = 1; i < n; ++i) {
+        int val = array[i];
+        int j = i - 1;
+        
+        while (j >= 0 && array[j] > val) {
+            array[j + 1] = array[j];
+            --j;
+        }
+        array[j + 1] = val;
+    }
+}
