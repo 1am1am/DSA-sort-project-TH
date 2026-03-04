77d73cecd1044d5deeda92f42e32e7431f7bf2ec
Canari <trinhbaokhoi@gmail.com>
2026-03-04 14:34:31 +0700

    Heap Sort, Shell Sort, Bubble Sort

---
+++ sortAlgorithms/bubbleSort.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+void bubbleSort(std::vector<int>&array, long long&comparisons) {
+    for (int i = 0 ; ++ comparisons && i < array.size() - 1; i++) {
+        for (int j = 0 ; ++ comparisons && j  < array.size() - i - 1 ; j++ ) {
+            if ( ++comparisons && array[j] > array[j+1] ) {
+                std::swap(array[j], array[j+1]);
+            }
+        }
+    }
+}
---
+++ sortAlgorithms/shellSort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+void shellSort(std::vector<int>&array,  long long&comparisons) {
+    for (int i = (array.size())/2; ++comparisons && i > 0 ; i/=2) {
+        for (int j = i; ++comparisons && j < array.size();j++) {
+            int tmp = array[j];
+            int k = j - i;
+            while ( ++ comparisons && k >= 0 && ++comparisons && array[k] > tmp ) {
+                array[k+i] = array[k];
+                k = k - i;
+            }
+            array[k+i] = tmp;
+
+        }
+    }
+}
