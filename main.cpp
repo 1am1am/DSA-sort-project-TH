f2ca8a8b4fbafad372793c9e081181b165fbd7e7
lamabas <dblam.hcmus@gmail.com>
2026-03-05 23:07:49 +0700

    Finish

--- main.cpp
+++ main.cpp
@@ -10,32 +10,11 @@
 int main(int argc, char* argv[]) {
     //g++ main.cpp -o 07.exe
 
-<<<<<<< HEAD
     std::vector<std::string> commandLine(argv, argv + argc);
 
     Command command = commandProcessing(argc, commandLine);
     getComparisonsAndTime(command);
 
     print(command);
-
-=======
-    // std::vector<std::string> commandLine(argv, argv + argc);
-    // commandProcessing(argc, commandLine);
-    string filename1 = "inputFiles/rev_10000.txt";
-    writeFile(filename1,"-rev",10000);
-    filename1 = "inputFiles/rev_30000.txt";
-    writeFile(filename1,"-rev",30000);
-    filename1 = "inputFiles/rev_50000.txt";
-    writeFile(filename1,"-rev",50000);
-    filename1 = "inputFiles/rev_100000.txt";
-    writeFile(filename1,"-rev",100000);
-    filename1 = "inputFiles/rev_300000.txt";
-    writeFile(filename1,"-rev",300000);
-    filename1 = "inputFiles/rev_500000.txt";
-    writeFile(filename1,"-rev",500000);
-    
-    cout << "check";
-    
->>>>>>> 26311d6d3973a60e66b7edefa3ad159c801809fe
     return 0;
 }
