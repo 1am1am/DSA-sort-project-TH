48d791e19613b4b0af0cd45ac387001797e90e83
lamabas <dblam.hcmus@gmail.com>
2026-03-05 17:00:35 +0700

    ...

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include "consoleUI.h"
 #include <iostream>
+#include <string>
+
+struct Input{
+    std::string mode;
+    std::string algorithm1;
+    std::string algorithm2;
+    std::string inputFile;
+    int inputSize;
+    std::string inputOrder;
+    std::string outputParameter;
+};
 
 int main(int argc, char* argv[]) {
     //g++ main.cpp -o 07.exe
     
+    Input input;
     
     return 0;
 }
