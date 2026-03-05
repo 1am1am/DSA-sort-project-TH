c0f9ab6f765cd0aed8b1443563116161b93a0ec6
Jin <rinma2405@gmail.com>
2026-03-05 16:58:39 +0700

    set mode maybe done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include "consoleUI.h"
 #include <iostream>
+#include "setMode.h"
 
 int main(int argc, char* argv[]) {
     //g++ main.cpp -o 07.exe
-    
-    
+    setMode(argv[0], )
     return 0;
 }
