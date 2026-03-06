37a35286c736a3171b6362828714f7653179baaa
thuan <nguyenhuungoct155@gmail.com>
2026-03-06 19:43:09 +0700

    add lai radixSort

--- source/sortAlgorithms/radixSort.cpp
+++ source/sortAlgorithms/radixSort.cpp
@@ -1,45 +1,52 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <ctime> 
 #include "sort.h"
 
 using namespace std;
-struct Bucket {
-	int* data;
-	int count;
-};
+
 void radixSort(vector<int>& array, long long& comparisons) {
-	int size = array.size();
-	int maxv = array[0];
-	for (int i = 0; ++comparisons && i < size;i++) {
-		if (++comparisons && maxv < array[i]) maxv = array[i];
-	}
-	Bucket buckets[10];
-	for (int i = 0;++comparisons && i < 10; i++) {
-		buckets[i].data = new int[size];
-	}
-	long long j = 1;
-	while (++comparisons && maxv / j > 0) {
-		for (int i = 0; ++comparisons && i < 10; i++) {
-			buckets[i].count = 0;
-		}
-		for (int i = 0; ++comparisons && i < size;i++) {
-			int digit = (array[i] / j % 10);
-			int empty = buckets[digit].count;
-			buckets[digit].data[empty] = array[i];
-			buckets[digit].count++;
-		}
-		int dex = 0;
-		for (int i = 0; ++comparisons && i < 10; i++) {
-			for (int l = 0; ++comparisons && l < buckets[i].count;l++) {
-				array[dex] = buckets[i].data[l];
-				dex++;
-			}
-		}
-		j *= 10;
-	}
-	for (int i = 0; ++comparisons && i < 10; i++) {
-		delete[] buckets[i].data;
-	}
+    if (array.empty()) return;
+
+    int size = array.size();
+    
+    int maxv = array[0];
+    for (int i = 1; ++comparisons && i < size; i++) {
+        if (maxv < array[i]) maxv = array[i];
+    }
+
+    long long j = 1;
+    while (maxv / j > 0) {
+        ++comparisons;
+        
+        int* Count = new int[10];
+        for (int i = 0; i < 10; ++i) Count[i] = 0;
+
+        for (int i = 0; ++comparisons && i < size; ++i) {
+            int digit = (array[i] / j) % 10;
+            ++Count[digit];
+        }
+
+        for (int i = 1; i < 10; ++i) {
+            Count[i] += Count[i - 1];
+        }
+
+        int* B = new int[size + 5];
+        
+        for (int i = size - 1; i >= 0; --i) {
+            int digit = (array[i] / j) % 10;
+            B[Count[digit] - 1] = array[i];
+            --Count[digit];
+        }
+
+        for (int i = 0; ++comparisons && i < size; ++i) {
+            array[i] = B[i];
+        }
+
+        delete[] Count;
+        delete[] B;
+        
+
+        j *= 10;
+    }
 }
