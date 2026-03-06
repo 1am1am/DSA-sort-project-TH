722e0a9eb8d213bfd106da1705ffb75809946b57
lamabas <dblam.hcmus@gmail.com>
2026-03-06 19:24:24 +0700

    done

--- source/sortAlgorithms/bubbleSort.cpp
+++ source/sortAlgorithms/bubbleSort.cpp
@@ -2,10 +2,10 @@
 #include <vector>
 #include "sort.h"
 void bubbleSort(std::vector<int>& array, long long& comparisons) {
-    for (int i = 0 ; ++comparisons && i < array.size() - 1; i++) {
-        for (int j = 0 ; ++comparisons && j  < array.size() - i - 1 ; j++) {
-            if (++comparisons && array[j] > array[j + 1]) {
-                std::swap(array[j], array[j + 1]);
+    for (int i = 0; ++comparisons && i < array.size(); i++) {
+        for (int j = array.size() - 1; ++comparisons && j > i; j--) {
+            if (++comparisons && array[j - 1] > array[j]) {
+                std::swap(array[j - 1], array[j]);
             }
         }
     }
