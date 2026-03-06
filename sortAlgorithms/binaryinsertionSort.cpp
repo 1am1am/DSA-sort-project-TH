f81fda51910eda822623396cf572bc2de71e8b53
Jin <rinma2405@gmail.com>
2026-03-06 16:40:47 +0700

    update khang sorts

--- sortAlgorithms/binaryinsertionSort.cpp
+++ sortAlgorithms/binaryinsertionSort.cpp
@@ -5,18 +5,18 @@
 using namespace std;
 
 void binaryInsertionSort(vector<int>& A, long long& comparisions){
-    for(int i = 1; i < A.size(); ++i){
+    for(int i = 1; ++comparisions && i < A.size(); ++i){
         int value = A[i];
         int l = 0, r = i - 1, pos = i - 1;
-        while(l <= r){
+        while( ++comparisions && l <= r){
             int m = l + ((r - l) >> 1);
-            if(A[m] >= value){
+            if( ++comparisions && A[m] >= value){
                 r = m - 1;
                 pos = m;
             }
             else l = m + 1;
         }
-        for(int j = i; j > pos; --j) A[j] = A[j - 1];
+        for(int j = i; ++comparisions && j > pos; --j) A[j] = A[j - 1];
         A[pos] = value;
     }
 }
--- sortAlgorithms/shakerSort.cpp
+++ sortAlgorithms/shakerSort.cpp
@@ -8,16 +8,16 @@ using namespace std;
 
 void shakerSort(vector<int>& A, long long& comparisions){
     int l = 0, r = A.size() - 1, pos = A.size() - 1;
-    while(l < r){
-        for(int i = l + 1; i <= r; ++i){
-            if(A[i] < A[i - 1]){
+    while(++comparisions && l < r){
+        for(int i = l + 1; ++comparisions && i <= r; ++i){
+            if(++comparisions && A[i] < A[i - 1]){
                 swap(A[i], A[i - 1]);
                 pos = i - 1;
             }
         }
         r = pos;
-        for(int i = r - 1; i >= l ; --i){
-            if(A[i] > A[i + 1]){
+        for(int i = r - 1; ++comparisions && i >= l ; --i){
+            if( ++comparisions && A[i] > A[i + 1]){
                 swap(A[i], A[i + 1]);
                 pos = i + 1;
             }
