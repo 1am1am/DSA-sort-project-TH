54e1212a16286569e9ab4e0fa8fb215f0425edf5
lamabas <dblam.hcmus@gmail.com>
2026-03-06 18:26:17 +0700

    fix

--- source/sortAlgorithms/flashSort.cpp
+++ source/sortAlgorithms/flashSort.cpp
@@ -5,52 +5,56 @@
 void flashSort(std::vector<int>& array, long long& comparisons) {
     int n = array.size();
     if (++comparisons && n <= 1) return;
+
     int m = 0.45 * n;
+    if (++comparisons && m < 1) m = 1;
     int* L = new int[m]{0};
-    int maxVal = array[0], minVal = array[0];
+
+    int minVal = array[0];
+    int maxIndex = 0;
 
     for (int i = 1; ++comparisons && i < n; ++i) {
-        maxVal = std::max(maxVal, array[i]);
-        minVal = std::min(minVal, array[i]);
+        if (++comparisons && array[i] < minVal) minVal = array[i];
+        if (++comparisons && array[i] > array[maxIndex]) maxIndex = i;
     }
 
-    if (++comparisons && minVal == maxVal) return;
+    if (++comparisons && array[maxIndex] == minVal) {
+        delete[] L;
+        return;
+    }
+
+    double c1 = (double)(m - 1) / (array[maxIndex] - minVal);
 
     for (int i = 0; ++comparisons && i < n; ++i) {
-        int k = 1.0 * (m - 1) * (array[i] - minVal) / (maxVal - minVal);
-        ++L[k];
+        int k = (int)(c1 * (array[i] - minVal));
+        L[k]++;
     }
 
     for (int i = 1; ++comparisons && i < m; ++i) {
         L[i] += L[i - 1];
     }
-    
-    int hold = array[0];
+
+    std::swap(array[0], array[maxIndex]); 
     int move = 0;
-    int flash = 0;
-    int k = 0;
-    int t = 0;
     int j = 0;
+    int k = m - 1;
 
     while (++comparisons && move < n - 1) {
-        while (++comparisons && j > (L[k] - 1)) {
-            ++j;
-            k = 1.0 * (m - 1) * (array[j] - minVal) / (maxVal - minVal);
+        while (++comparisons && j >= L[k]) {
+            j++;
+            k = (int)(c1 * (array[j] - minVal));
         }
-
-        flash = array[j];
+        
+        int flash = array[j];
         while (++comparisons && j != L[k]) {
-            k = 1.0 * (m - 1) * (array[j] - minVal) / (maxVal - minVal);
-            hold = array[t=--L[k]];
-            array[t] = hold;
-            flash = hold;
-            ++move;
-
+            k = (int)(c1 * (flash - minVal));
+            int pos = --L[k];
+            std::swap(flash, array[pos]);
+            move++;
         }
     }
 
-    insertionSort(array, comparisons);
-
     delete[] L;
     L = nullptr;
+    insertionSort(array, comparisons);
 }
