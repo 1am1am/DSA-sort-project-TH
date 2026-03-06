401f663055884eb04567fa2fa07e38415afca40d
Canari <trinhbaokhoi@gmail.com>
2026-03-07 02:11:02 +0700

    da sua heap sort

--- source/sortAlgorithms/heapSort.cpp
+++ source/sortAlgorithms/heapSort.cpp
@@ -2,33 +2,37 @@
 #include <vector>
 #include "sort.h"
 
-void Heapify(std::vector<int>& array , int start, int n, long long& comparisons) {
-    int leftChild = start *  2 + 1;
+void Heapify(std::vector<int>& array, int start, int n, long long& comparisons) {
+    int leftChild = start * 2 + 1;
     if (++comparisons && leftChild >= n) return;
+
     int largerChild = leftChild;
     int rightChild = start * 2 + 2;
+
     if (++comparisons && rightChild < n) {
         if (++comparisons && array[rightChild] > array[leftChild]) {
             largerChild = rightChild;
         }
     }
 
-    if (++comparisons && array[start] < array[largerChild] ) {
+    if (++comparisons && array[start] < array[largerChild]) {
         std::swap(array[start], array[largerChild]);
-        Heapify(array,largerChild,n,comparisons);
+        Heapify(array, largerChild, n, comparisons);
     }
 }
 
-
 void heapSort(std::vector<int>& array, long long& comparisons) {
-    for (int i = (array.size()-1)/2; ++comparisons && i >= 0 ; i--) {
-        Heapify(array,i,array.size(),comparisons);
+    int n = array.size();
+
+    for (int i = n / 2 - 1; ++comparisons && i >= 0; i--) {
+        Heapify(array, i, n, comparisons);
     }
-    std::swap(array[0], array[array.size()-1]);
-    int HeapSize = array.size()-1;
-    while (++comparisons && HeapSize > 1) {
-        Heapify(array,0,HeapSize, comparisons);
-        HeapSize--;
-        std::swap(array[0], array[HeapSize]);
+
+    int heapSize = n;
+
+    while (++comparisons && heapSize > 1) {
+        std::swap(array[0], array[heapSize - 1]); 
+        heapSize--;
+        Heapify(array, 0, heapSize, comparisons);
     }
 }
