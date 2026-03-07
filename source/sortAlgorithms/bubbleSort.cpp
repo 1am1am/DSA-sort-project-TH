249b31ecc527064133dc3d780c3424acbcf78956
Canari <trinhbaokhoi@gmail.com>
2026-03-07 17:39:33 +0700

    da sua bubble

--- source/sortAlgorithms/bubbleSort.cpp
+++ source/sortAlgorithms/bubbleSort.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <vector>
 #include "sort.h"
+
+using namespace std;
 void bubbleSort(std::vector<int>& array, long long& comparisons) {
-    for (int i = 0; ++comparisons && i < array.size(); i++) {
-        for (int j = array.size() - 1; ++comparisons && j > i; j--) {
-            if (++comparisons && array[j - 1] > array[j]) {
-                std::swap(array[j - 1], array[j]);
+    bool unsorted = true;
+    int pas = 0 ; 
+    int n = array.size();
+    while (unsorted) {
+        unsorted = false;
+        pas++;
+        for (int i = 0 ; ++ comparisons && i < n - pas; i++ ) {
+            if (++ comparisons && array[i] > array[i+1]) {
+                swap(array[i], array[i+1]);
+                unsorted = true;    
             }
         }
     }
