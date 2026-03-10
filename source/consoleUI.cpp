7b27ddc0c188866beeb6917042f45429e5fddf4e
lamabas <dblam.hcmus@gmail.com>
2026-03-10 21:43:30 +0700

    Comparisions not Comparisons

--- source/consoleUI.cpp
+++ source/consoleUI.cpp
@@ -10,10 +10,10 @@ void outputParameter(std::string outputType, Command command) {
     if (outputType == "-time") {
         std::cout << "Running time: " << command.runningTime1 << (!command.algorithm2.empty() ? " | " : "") << (!command.algorithm2.empty() ? std::to_string(command.runningTime2) : "") << '\n';
     } else if (outputType == "-comp") {
-        std::cout << "Comparisons: " << command.comparisons1 << (!command.algorithm2.empty() ? " | " : "") << (!command.algorithm2.empty() ? std::to_string(command.comparisons2) : "") << '\n';
+        std::cout << "Comparisions: " << command.comparisons1 << (!command.algorithm2.empty() ? " | " : "") << (!command.algorithm2.empty() ? std::to_string(command.comparisons2) : "") << '\n';
     } else {
         std::cout << "Running time: " << command.runningTime1 << (!command.algorithm2.empty() ? " | " : "") << (!command.algorithm2.empty() ? std::to_string(command.runningTime2) : "") << '\n';
-        std::cout << "Comparisons: " << command.comparisons1 << (!command.algorithm2.empty() ? " | " : "") << (!command.algorithm2.empty() ? std::to_string(command.comparisons2) : "") << '\n';
+        std::cout << "Comparisions: " << command.comparisons1 << (!command.algorithm2.empty() ? " | " : "") << (!command.algorithm2.empty() ? std::to_string(command.comparisons2) : "") << '\n';
     }
 }
 
@@ -42,10 +42,10 @@ std::string nameAlgorithm(std::string algorithm) {
 }
 
 void consoleUI(Command command) {
-    std::cout << "Algorithm: " << nameAlgorithm(command.algorithm1) << (!command.algorithm2.empty() ? " | " : "") << nameAlgorithm(command.algorithm2) << '\n';
+    std::cout << "Algorithm: " << command.algorithm1 << (!command.algorithm2.empty() ? " | " : "") << command.algorithm2 << '\n';
     if (!command.inputFile.empty()) std::cout << "Input file: " << command.inputFile << '\n';
     std::cout << "Input size: " << command.inputSize << '\n';
-    if (command.inputFile.empty()) std::cout << "Input order: " << algorithmInputOrder(command.inputOrder) << '\n';
+    if (command.inputFile.empty()) std::cout << "Input order: " << command.inputOrder << '\n';
     std::cout << "-------------------------\n";
     outputParameter(command.outputParameter, command);
 }
