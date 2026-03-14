91d19504a7d54fa874c930932f4f48648c380f84
thuan <nguyenhuungoct155@gmail.com>
2026-03-15 00:38:39 +0700

    update cai writing file cm3 cm5

--- source/getComparisonsAndTime.cpp
+++ source/getComparisonsAndTime.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-long long getAlgorithm(string Name, const vector<int>& Array,double &Time){
+long long getAlgorithm(string Name, const vector<int>& Array,double &Time,vector<int>& SortedArray){
     vector<int> Tmp = Array;
     long long Comparisons = 0;
     auto start = chrono::high_resolution_clock::now();
@@ -91,7 +91,7 @@ long long getAlgorithm(string Name, const vector<int>& Array,double &Time){
 
     chrono::duration<double, milli> duration = end - start;
     Time = duration.count();
-
+    SortedArray = Tmp;
     return Comparisons;
 }
 
@@ -118,15 +118,80 @@ void makeInput(vector<int>& Array, Command& command){
     if(command.inputOrder == "-nsorted") Array = GenerateData(command.inputSize, "-nsorted");
 }
 
+
+long long getAlgorithm(string Name, const vector<int>& Array, double &Time, vector<int>& SortedArray){
+    vector<int> Tmp = Array;
+    long long Comparisons = 0;
+    auto start = chrono::high_resolution_clock::now();
+    auto end = chrono::high_resolution_clock::now();
+
+    if("selection-sort" == Name){
+        selectionSortTime(Tmp);
+        end = chrono::high_resolution_clock::now();
+        Tmp = Array;
+        selectionSort(Tmp, Comparisons);
+    }
+    else if("insertion-sort" == Name){
+        insertionSortTime(Tmp);
+        end = chrono::high_resolution_clock::now();
+        Tmp = Array;
+        insertionSort(Tmp, Comparisons);
+    }
+
+    else if("flash-sort" == Name){
+        flashSortTime(Tmp);
+        end = chrono::high_resolution_clock::now();
+        Tmp = Array;
+        flashSort(Tmp, Comparisons);
+    }
+
+    chrono::duration<double, milli> duration = end - start;
+    Time = duration.count();
+    
+
+    SortedArray = Tmp; 
+
+    return Comparisons;
+}
+
+
+
 void getComparisonsAndTime(Command& command){
+
+    if (command.inputOrder == "-all") return; 
+
     double time1 = 0.0, time2 = 0.0;
     vector<int> Array; 
-    if(command.inputFile != "") readFile(Array, command);
-    else makeInput(Array, command);
-    long long Comparison1 = getAlgorithm(command.algorithm1, Array,time1), Comparison2 = 0;
-    if(command.mode == "-c") Comparison2 = getAlgorithm(command.algorithm2, Array,time2);
+    
+    if(command.inputFile != "") {
+        readFile(Array, command);
+    } else {
+        makeInput(Array, command);
+        
+
+        if (command.isCommand3) {
+
+            if (command.inputOrder == "-rand") writeArrayToFile("input_1.txt", command.inputSize, Array);
+            else if (command.inputOrder == "-nsorted") writeArrayToFile("input_2.txt", command.inputSize, Array);
+            else if (command.inputOrder == "-sorted") writeArrayToFile("input_3.txt", command.inputSize, Array);
+            else if (command.inputOrder == "-rev") writeArrayToFile("input_4.txt", command.inputSize, Array);
+        } else {
+
+            writeArrayToFile("input.txt", command.inputSize, Array);
+        }
+    }
+    
+    vector<int> SortedArray1, SortedArray2;
+    long long Comparison1 = getAlgorithm(command.algorithm1, Array, time1, SortedArray1), Comparison2 = 0;
+    if(command.mode == "-c") Comparison2 = getAlgorithm(command.algorithm2, Array, time2, SortedArray2);
+    
     command.comparisons1 = Comparison1;
     command.comparisons2 = Comparison2;
     command.runningTime1 = time1;
     command.runningTime2 = time2;
+
+
+    if (command.mode == "-a" && !command.isCommand3) {
+        writeArrayToFile("output.txt", command.inputSize, SortedArray1);
+    }
 }
