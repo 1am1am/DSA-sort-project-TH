d0543b5e0da397f4a2259eca272d4067b792ebea
thuan <nguyenhuungoct155@gmail.com>
2026-03-14 23:29:25 +0700

    add report

--- source/sortAlgorithms/quickSort.cpp
+++ source/sortAlgorithms/quickSort.cpp
@@ -1,141 +1,69 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <ctime>
-#include <cstdlib>
 #include "sort.h"
 
 using namespace std;
 
-void insertionSortSubmatrix(std::vector<int>& array, int l, int r, long long& comparisons) {
-    int n = array.size();
-    for (int i = l + 1; ++comparisons && i <= r; ++i) {
-        int val = array[i];
-        int j = i - 1;
-        
-        while (++comparisons && j >= 0 && ++comparisons && array[j] > val) {
-            array[j + 1] = array[j];
-            --j;
-        }
-        array[j + 1] = val;
-    }
-}
-
-int selectPivot(vector<int>& arr, int l, int r, long long& comparisons) {
+int partition(vector<int>& arr, int l, int r, long long& comparisons) { 
     int mid = l + (r - l) / 2;
-    if (++comparisons && arr[l] > arr[mid]) std::swap(arr[l], arr[mid]);
-    if (++comparisons && arr[mid] > arr[r]) std::swap(arr[mid], arr[r]);
-    if (++comparisons && arr[l] > arr[mid]) std::swap(arr[l], arr[mid]);
-    return mid;
-}
+    swap(arr[mid], arr[r]); 
 
-int partition(vector<int>& arr, int l, int r, long long& comparisons) { // lomuto
-    int pivotIdx = selectPivot(arr, l, r, comparisons);
-    int pivot = arr[pivotIdx];
-
-    std::swap(arr[pivotIdx], arr[r - 1]);
-    pivotIdx = r - 1;
-
-    int left = l + 1;
-    int right = r - 2;
-
-    while (++comparisons && left <= right) {
-        while (++comparisons && arr[left] < pivot) left++;
-        while (++comparisons && arr[right] > pivot) right--;
-
-        if (++comparisons && left <= right) {
-            std::swap(arr[left], arr[right]);
-            left++;
-            right--;
+    int pivot = arr[r];
+    int j = l - 1;
+    for (int i = l; ++comparisons && i < r; i++) {
+        if (++comparisons && arr[i] <= pivot) {
+            j++;
+            swap(arr[j], arr[i]);
         }
     }
-
-    std::swap(arr[pivotIdx], arr[left]);
-    pivotIdx = left;
-    return pivotIdx;
+    j++;
+    swap(arr[j], arr[r]);
+    return j;
 }
 
-void quickSortArray(vector<int>& array, int l, int r, long long& comparisons) { // lomuto
+void quickSortArray(vector<int>& array, int l, int r, long long& comparisons) { 
     if (++comparisons && l < r) {
-        if (++comparisons && (r - l) < 10) {
-            insertionSortSubmatrix(array, l, r, comparisons);
-            return;
-        }
         int p = partition(array, l, r, comparisons);
         quickSortArray(array, l, p - 1, comparisons);
         quickSortArray(array, p + 1, r, comparisons);
     }
 }
-void quickSort(vector<int>& array, long long& comparisons) { // lomuto
-    if (++comparisons && !array.empty()) {
+
+void quickSort(vector<int>& array, long long& comparisons) { 
+    comparisons = 0; 
+    if (!array.empty()) {
         quickSortArray(array, 0, array.size() - 1, comparisons);
     }
 }
 
 
-
-
-
-void insertionSortSubmatrixTime(std::vector<int>& array, int l, int r) {
-    int n = array.size();
-    for (int i = l + 1; i <= r; ++i) {
-        int val = array[i];
-        int j = i - 1;
-        
-        while (j >= 0 && array[j] > val) {
-            array[j + 1] = array[j];
-            --j;
-        }
-        array[j + 1] = val;
-    }
-}
-
-int selectPivotTime(vector<int>& arr, int l, int r) {
+int partitionTime(vector<int>& arr, int l, int r) { 
     int mid = l + (r - l) / 2;
-    if (arr[l] > arr[mid]) std::swap(arr[l], arr[mid]);
-    if (arr[mid] > arr[r]) std::swap(arr[mid], arr[r]);
-    if (arr[l] > arr[mid]) std::swap(arr[l], arr[mid]);
-    return mid;
-}
+    swap(arr[mid], arr[r]); 
 
-int partitionTime(vector<int>& arr, int l, int r) { // lomuto
-    int pivotIdx = selectPivotTime(arr, l, r);
-    int pivot = arr[pivotIdx];
-
-    std::swap(arr[pivotIdx], arr[r - 1]);
-    pivotIdx = r - 1;
-
-    int left = l + 1;
-    int right = r - 2;
-
-    while (left <= right) {
-        while (arr[left] < pivot) left++;
-        while (arr[right] > pivot) right--;
-
-        if (left <= right) {
-            std::swap(arr[left], arr[right]);
-            left++;
-            right--;
+    int pivot = arr[r];
+    int j = l - 1;
+    for (int i = l; i < r; i++) {
+        if (arr[i] <= pivot) {
+            j++;
+            swap(arr[j], arr[i]);
         }
     }
-
-    std::swap(arr[pivotIdx], arr[left]);
-    pivotIdx = left;
-    return pivotIdx;
+    j++;
+    swap(arr[j], arr[r]);
+    return j;
 }
 
-void quickSortArrayTime(vector<int>& array, int l, int r) { // lomuto
+void quickSortArrayTime(vector<int>& array, int l, int r) { 
     if (l < r) {
-        if ((r - l) < 10) {
-            insertionSortSubmatrixTime(array, l, r);
-            return;
-        }
         int p = partitionTime(array, l, r);
         quickSortArrayTime(array, l, p - 1);
         quickSortArrayTime(array, p + 1, r);
     }
 }
-void quickSortTime(vector<int>& array) { // lomuto
+
+void quickSortTime(vector<int>& array) { 
     if (!array.empty()) {
         quickSortArrayTime(array, 0, array.size() - 1);
     }
--- source/sortAlgorithms/radixSort.cpp
+++ source/sortAlgorithms/radixSort.cpp
@@ -4,93 +4,88 @@
 #include "sort.h"
 
 using namespace std;
+struct Bucket {
+    int* data;
+    int count;
+};
+void radixSort(vector<int>& arr, long long& comparisons) {
+    int size = arr.size();
+    if (++comparisons && size <= 0) return; 
 
-void radixSort(vector<int>& array, long long& comparisons) {
-    if (array.empty()) return;
-
-    int size = array.size();
-    
-    int maxv = array[0];
+    int maxv = arr[0];
     for (int i = 1; ++comparisons && i < size; i++) {
-        if (maxv < array[i]) maxv = array[i];
+        if (++comparisons && maxv < arr[i]) maxv = arr[i];
     }
 
-    long long j = 1;
+    Bucket buckets[10];
+    for (int i = 0;++comparisons && i < 10; i++) {
+        buckets[i].data = new int[size];
+    }
+
+    long long j = 1; 
     while (maxv / j > 0) {
         ++comparisons;
-        
-        int* Count = new int[10];
-        for (int i = 0; i < 10; ++i) Count[i] = 0;
-
-        for (int i = 0; ++comparisons && i < size; ++i) {
-            int digit = (array[i] / j) % 10;
-            ++Count[digit];
+        for (int i = 0;++comparisons && i < 10; i++) {
+            buckets[i].count = 0;
         }
 
-        for (int i = 1; i < 10; ++i) {
-            Count[i] += Count[i - 1];
+        for (int i = 0;++comparisons && i < size; i++) {
+            int digit = (arr[i] / j % 10);
+            int empty = buckets[digit].count;
+            buckets[digit].data[empty] = arr[i];
+            buckets[digit].count++;
         }
 
-        int* B = new int[size + 5];
-        
-        for (int i = size - 1; i >= 0; --i) {
-            int digit = (array[i] / j) % 10;
-            B[Count[digit] - 1] = array[i];
-            --Count[digit];
+        int dex = 0;
+        for (int i = 0; ++comparisons && i < 10; i++) {
+            for (int k = 0; k < buckets[i].count; k++) { 
+                arr[dex] = buckets[i].data[k];
+                dex++;
+            }
         }
-
-        for (int i = 0; ++comparisons && i < size; ++i) {
-            array[i] = B[i];
-        }
-
-        delete[] Count;
-        delete[] B;
-        
-
         j *= 10;
+    }
+    for (int i = 0; i < 10; i++) {
+        delete[] buckets[i].data;
     }
 }
+void radixSortTime(vector<int>& arr) {
+    int size = arr.size();
+    if (size <= 0) return; 
 
-void radixSortTime(vector<int>& array) {
-    if (array.empty()) return;
-
-    int size = array.size();
-    
-    int maxv = array[0];
-    for (int i = 1; i < size; i++) {
-        if (maxv < array[i]) maxv = array[i];
+    int maxv = arr[0];
+    for (int i = 1;  i < size; i++) {
+        if (maxv < arr[i]) maxv = arr[i];
     }
 
-    long long j = 1;
-    while (maxv / j > 0) {       
-        int* Count = new int[10];
-        for (int i = 0; i < 10; ++i) Count[i] = 0;
+    Bucket buckets[10];
+    for (int i = 0; i < 10; i++) {
+        buckets[i].data = new int[size];
+    }
 
-        for (int i = 0; i < size; ++i) {
-            int digit = (array[i] / j) % 10;
-            ++Count[digit];
+    long long j = 1; 
+    while (maxv / j > 0) {
+        for (int i = 0; i < 10; i++) {
+            buckets[i].count = 0;
         }
 
-        for (int i = 1; i < 10; ++i) {
-            Count[i] += Count[i - 1];
+        for (int i = 0; i < size; i++) {
+            int digit = (arr[i] / j % 10);
+            int empty = buckets[digit].count;
+            buckets[digit].data[empty] = arr[i];
+            buckets[digit].count++;
         }
 
-        int* B = new int[size + 5];
-        
-        for (int i = size - 1; i >= 0; --i) {
-            int digit = (array[i] / j) % 10;
-            B[Count[digit] - 1] = array[i];
-            --Count[digit];
+        int dex = 0;
+        for (int i = 0; i < 10; i++) {
+            for (int k = 0; k < buckets[i].count; k++) { 
+                arr[dex] = buckets[i].data[k];
+                dex++;
+            }
         }
-
-        for (int i = 0; i < size; ++i) {
-            array[i] = B[i];
-        }
-
-        delete[] Count;
-        delete[] B;
-        
-
         j *= 10;
+    }
+    for (int i = 0; i < 10; i++) {
+        delete[] buckets[i].data;
     }
 }
