886ad1ccdac008b684108b91e46693213fa03945
Jin <rinma2405@gmail.com>
2026-03-15 11:05:04 +0700

    update

--- source/sortAlgorithms/shakerSort.cpp
+++ source/sortAlgorithms/shakerSort.cpp
@@ -7,41 +7,45 @@
 using namespace std;
 
 void shakerSort(vector<int>& A, long long& comparisions){
-    int l = 0, r = A.size() - 1, pos = A.size() - 1;
-    while(++comparisions && l < r){
-        for(int i = l + 1; ++comparisions && i <= r; ++i){
+    bool swapped = 1;
+    while(++comparisions && swapped){
+        swapped = 0;
+        for(int i = 0; ++comparisions && i < A.size(); ++i){
             if(++comparisions && A[i] < A[i - 1]){
                 swap(A[i], A[i - 1]);
-                pos = i - 1;
+                swapped = 1;
             }
         }
-        r = pos;
-        for(int i = r - 1; ++comparisions && i >= l ; --i){
+        if(++comparisions && !swapped) break;
+
+        swapped = 0;
+        for(int i = A.size() - 1; ++comparisions && i >= 0 ; --i){
             if( ++comparisions && A[i] > A[i + 1]){
                 swap(A[i], A[i + 1]);
-                pos = i + 1;
+                swapped = 1;
             }
         }
-        l = pos;
     }
 }
 
 void shakerSortTime(vector<int>& A){
-    int l = 0, r = A.size() - 1, pos = A.size() - 1;
-    while(l < r){
-        for(int i = l + 1; i <= r; ++i){
+   bool swapped = 1;
+    while(swapped){
+        swapped = 0;
+        for(int i = 0; i < A.size(); ++i){
             if(A[i] < A[i - 1]){
                 swap(A[i], A[i - 1]);
-                pos = i - 1;
+                swapped = 1;
             }
         }
-        r = pos;
-        for(int i = r - 1; i >= l ; --i){
+        if(!swapped) break;
+
+        swapped = 0;
+        for(int i = A.size() - 1; i >= 0 ; --i){
             if(A[i] > A[i + 1]){
                 swap(A[i], A[i + 1]);
-                pos = i + 1;
+                swapped = 1;
             }
         }
-        l = pos;
     }
 }
